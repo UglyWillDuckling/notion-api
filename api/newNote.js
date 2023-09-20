34ada586e94946919f3184079fb180856674a2af
vladimir <vladimi.sedlar@monsoonconsulting.com>
2023-09-20 16:44:07 +0200

    api: use editor input for description
    #description #editor #api

--- api/newNote.js
+++ api/newNote.js
@@ -1,13 +1,12 @@
-import chalk from 'chalk'
-import rl  from 'readline'
-import { createSelect } from './select.js'
-import { Client } from "@notionhq/client"
+#!/usr/bin/env node
 
+import { Client } from "@notionhq/client"
 import { input } from '@inquirer/prompts';
 import select, { Separator } from '@inquirer/select';
+import editor from '@inquirer/editor';
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
-const database_map = [ {
+const database_map = [{
   value: '4d453679bd1b4b6f9f316e46a17538ff',
   name: 'Notes'
 },
@@ -17,27 +16,7 @@ const database_map = [ {
   }
 ]
 
-function prompt(question, opts) {
-  const r = rl.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-  })
-
-  return new Promise((resolve) => {
-    if(opts) {
-      const select = createSelect(opts, resolve)
-      select.init()
-    } else {
-      r.question(question, answer => {
-        r.close()
-        resolve(answer)
-      })
-    }
-  })
-}
-
-async function addNoteToDB (note, databaseId, opts) {
+async function addNoteToDB (note, databaseId, opts = {}) {
   try {
     const response = await notion.pages.create({
       parent: { database_id: databaseId },
@@ -82,7 +61,7 @@ async function addNoteToDB (note, databaseId, opts) {
 async function addItem() {
   const chosenDB = await select({ message: 'Select the db you wish to add to', choices: database_map })
   const title = await input({ message: 'Enter your title' })
-  const content = await input({ message: 'Enter your content' })
+  const content = await editor({ message: 'Enter your content', postfix: '.md' })
   // console.log(`chosen db ${chosenDB}, title: ${title}, content: ${content}`)
 
   addNoteToDB(
@@ -91,7 +70,7 @@ async function addItem() {
       content: content
     },
     chosenDB,
-    { debug: true }
+    // { debug: true }
   )
 }
 
