aff7cdbcfe469e9a30c98948d1b6933c0dd543d2
vladimir <vlado.sedlar@gmail.com>
2024-06-07 22:40:36 +0200

    fix: cover edge cases when some of the props are not added

--- api/newNote.js
+++ api/newNote.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// TODO
 import { Client } from "@notionhq/client"
 import { input } from '@inquirer/prompts';
 import select, { Separator } from '@inquirer/select';
@@ -11,48 +12,52 @@ const database_map = [{
   value: '4d453679bd1b4b6f9f316e46a17538ff',
   name: 'Notes'
 },
-  {
-    value: '93b229cbc6674f0f80df75d710c874c9',
-    name: 'Knowledge'
-  }
+{
+  value: '93b229cbc6674f0f80df75d710c874c9',
+  name: 'Knowledge'
+}
 ]
 
-async function addNoteToDB (note, databaseId, opts = {}, errHandler = () => {}) {
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      icon: { "type": "emoji", "emoji": note.icon || "🗒" },
-      properties: {
-          Labels: {
-            multi_select: note.labels,
-          },
-          title: { title:[ { "text": { "content": note.title } } ]
-        },
-      },
-      "children": markdownToBlocks(note.content)
-    })
+async function addNoteToDB(note, databaseId, opts = {}, errHandler = () => { }) {
+  let properties = {
+    title: { title: [{ "text": { "content": note.title } }] },
+  }
+  if (note.labels.length) {
+    properties.Labels = { multi_select: note.labels }
+  }
 
-    console.log(`New Note Added!!! 🚀\nURL: ${response.url}`)
-    if(opts.debug) console.log(response)
-  } catch (error) {
-    errHandler(error)
+  let page = { parent: { database_id: databaseId }, properties, children: markdownToBlocks(note.content) }
+
+  if (note.icon) {
+    page.icon = { "type": "emoji", "emoji": note.icon }
+  }
+
+  try {
+    const response = await notion.pages.create(page)
+    console.log(`Nen Note Added!!! 🚀\nURL: ${response.url}`)
+    if (opts.debug) console.log(response)
+  } catch (err) {
+    console.log(err)
+    errHandler(err)
   }
 }
 
 async function addItem() {
-  function send () {  addNoteToDB(note, chosenDB, {}, errHandler) }
+  function send() { addNoteToDB(note, chosenDB, {}, errHandler) }
 
   const chosenDB = await select({ message: 'Select the db you wish to add to', choices: database_map })
   const title = await input({ message: 'Enter your title' })
-  const icon = await input({ message: 'Enter ICON' })
+  let icon = await input({ message: 'Enter ICON' })
+  icon = icon ? icon : '🗒'
   const content = await editor({ message: 'Enter your content', postfix: '.md' })
-  const labels = await input({ message: 'Enter labels separated by a comma(,)', postfix: '.md' }).then((text) => text.split(',').map((value) => { return {name: value} }))
+  const labels = await input({ message: 'Enter labels separated by a comma(,)', postfix: '.md' })
+    .then((text) => text.split(',').filter((value) => !!value).map((value) => { return { name: value } }))
 
   const note = { title: title, content: content, icon, labels }
 
   const errHandler = (err) => {
-    select({ message: 'Do you want to try again?', choices: [{value: 1, name: 'Yes'}, {value: 0, name: 'No'}] }).then((answer) => {
-      if(answer) send()
+    select({ message: 'Do you want to try again?', choices: [{ value: 1, name: 'Yes' }, { value: 0, name: 'No' }] }).then((answer) => {
+      if (answer) send()
       else console.log(err)
     })
   }
@@ -60,5 +65,6 @@ async function addItem() {
   send()
 }
 
+// test()
 addItem()
 
