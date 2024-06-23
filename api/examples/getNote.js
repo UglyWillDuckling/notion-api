65c1f9ef5781ba50ff4a1964d0d432e9bf1232c9
vladimir <vlado.sedlar@gmail.com>
2024-06-23 21:34:16 +0200

    chore: update example for new note

--- api/examples/getNote.js
+++ api/examples/getNote.js
@@ -1,20 +1,16 @@
 import { Client } from "@notionhq/client"
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
+const databaseId = '4d453679bd1b4b6f9f316e46a17538ff'
 
-const databaseId = process.env.NOTION_DATABASE_ID
+async function getCourses() {
+  const { results: page } = await notion.databases.query({
+    database_id: databaseId,
+  })
 
-async function getCourses()  {
-  try {
-    const { results } = await notion.databases.query({
-      database_id: databaseId,
-    });
-    const page = results[0]
-
-    return page
-  } catch (error) {
-    console.error(error)
-  }
+  return page
 }
 
-getCourses().then((something) => console.log(something))
+const newPage = await getCourses()
+console.log(newPage)
+
